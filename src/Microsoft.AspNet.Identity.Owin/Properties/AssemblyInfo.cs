ec56ada4d28a741ff741429ad828b4fa6d229262
Federico Dipuma <federicodipuma@gipo.it>
2020-10-15 19:05:33 +0200

    migrated owin project to new csproj format

--- src/Microsoft.AspNet.Identity.Owin/Properties/AssemblyInfo.cs
+++ src/Microsoft.AspNet.Identity.Owin/Properties/AssemblyInfo.cs
@@ -6,19 +6,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyTitle("Microsoft.AspNet.Identity.Owin")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Microsoft.AspNet.Identity.Owin")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -41,8 +28,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
 [assembly:
     InternalsVisibleTo(
